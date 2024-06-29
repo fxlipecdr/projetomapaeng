e49e11ecfa24d3e9106cc179a972d16277845acb
fxlipecdr <fxlipecdr@gmail.com>
2024-06-28 21:04:28 -0300

    Primeiro commit: Adicionando projeto MeuPrograma

---
+++ src/MeuPrograma.java
@@ -0,0 +1,11 @@
+public class MeuPrograma {
+    public static void main(String[] args) {
+        // Declaração de variáveis
+        int numero1 = 10;
+        int numero2 = 20;
+        int soma = numero1 + numero2;
+
+        // Exibindo a soma no console
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
+    }
+}
